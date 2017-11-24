3b34540defbed5603f9d8d4c555d8cffdea0e91c
JimmyLin39 <jimmylin0309@email.com>
2017-11-24 12:36:17 -0800

    delete unusable code and add /resources/:id router working

--- routes/resources.js
+++ routes/resources.js
@@ -5,38 +5,28 @@ const router = express.Router();
 
 module.exports = (knex) => {
 
-  router.get("/", (req, res) => {
-    // knex
-    //   .select('*').from('resources')
-    //   // .then(() => {
-    //   //   knex.select('tag_name').from('tags')
-    //   //       .join()
-    //   //     .where('resource_id', )
-    //   // })
-      
-    //   .then((results) => {
-    //     res.json(results);
-    //   });
-    
+  router.get("/", (req, res) => {    
     knex('resources').join('tags','resources.id','tags.resource_id')
     .select('resources.title', 'resources.id', 'tags.tag_name')
     //.where('resources.id','=',3)
     .then((result)=>{
       res.send(result);
     });
+    res.render('index'); 
   });
 
   // new resource page with comment
   router.get('/:id', (req, res) => {
-    console.log('i am here.');
-    const resource_id = req.params.id;
-    console.log(resource_id);
     knex
       .select("*")
       .from("resources")
-      .where('id', resource_id)
-      .then((results) => {
-        res.send(results);
+      .where('id', req.params.id)
+      .then((resources) => {
+        if (resources.length) {
+          res.render('resources_id', { resource: resources[0] });
+        } else {
+          res.send(404);
+        }
       });
   });
 
